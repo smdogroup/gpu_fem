a3304c39363fc8e947dbf994eecddf2e4eb4f9c6
seanfireball1 <sean.engelstad1@gmail.com>
2025-11-04 23:11:44 -0500

    new qorder directory

---
+++ examples/qorder/1_plate.cu
@@ -0,0 +1,160 @@
+#include "../plate/_src/_plate_utils.h"
+#include "linalg/_linalg.h"
+#include "solvers/_solvers.h"
+#include "mesh/TACSMeshLoader.h"
+#include "mesh/vtk_writer.h"
+
+// shell imports
+#include "element/shell/director/linear_rotation.h"
+#include "element/shell/physics/isotropic_shell.h"
+
+// lagrange MITC element
+#include "element/shell/basis/lagrange_basis.h"
+#include "element/shell/mitc_shell.h"
+
+/* command line args:
+    [linear|nonlinear] [--iterative] [--nxe int]
+    * currently nxe must be multiple of 5
+
+    examples:
+    ./static.out linear --iterative --nxe 10    to run linear, GMRES solve with nxe=10
+    ./static.out nonlinear   to run nonlinear
+    if --iterative is not used, full_LU direct solve instead
+*/
+
+void solve_linear(double slenderness, int nxe, double qorder) {
+    using T = double;   
+    using Quad = QuadLinearQuadrature<T>;
+    using Director = LinearizedRotation<T>;
+    using Basis = LagrangeQuadBasis<T, Quad, 2>;
+    using Geo = Basis::Geo;
+
+    constexpr bool has_ref_axis = false;
+    constexpr bool is_nonlinear = false;
+    using Data = ShellIsotropicData<T, has_ref_axis>;
+    using Physics = IsotropicShell<T, Data, is_nonlinear>;
+    using Assembler = MITCShellAssembler<T, Director, Basis, Physics, DeviceVec, BsrMat>;
+
+    int nye = nxe;
+    double Lx = 1.0, Ly = 1.0, E = 70e9, nu = 0.3, thick = 1.0 / slenderness, rho = 2500, ys = 350e6;
+    int nxe_per_comp = nxe / 5, nye_per_comp = nye/5; // for now (should have 25 grids)
+    auto assembler = createPlateAssembler<Assembler>(nxe, nye, Lx, Ly, E, nu, thick, rho, ys, nxe_per_comp, nye_per_comp);
+
+    // BSR symbolic factorization
+    // must pass by ref to not corrupt pointers
+    auto& bsr_data = assembler.getBsrData();
+    bool print = true;
+
+    /*
+    RCM and reorderings actually hurt GMRES performance on the plate case
+    because the matrix already has a nice banded structure => RCM increases bandwidth (which means it just doesn't work well for this problem
+    as it's whole point is to decrease matrix bandwidth)
+    */
+
+    // bsr_data.AMD_reordering();
+    // bsr_data.RCM_reordering();
+
+    if (qorder > 0) {
+        printf("Qorder with p = %.4e\n", qorder);
+        bsr_data.qorder_reordering(qorder);
+    } else {
+        printf("Qorder input < 0, so no qorder\n");
+    }
+        
+    // bsr_data.compute_ILUk_pattern(5, fillin);
+    bsr_data.compute_nofill_pattern();
+    // bsr_data.compute_ILUk_pattern(0, fillin);
+    // bsr_data.compute_full_LU_pattern(fillin, print); // reordered full LU here for debug
+
+    // printf("perm:");
+    // printVec<int>(bsr_data.nnodes, bsr_data.perm);
+    assembler.moveBsrDataToDevice();
+
+    // get the loads
+    double Q = 1.0; // load magnitude
+    // T *my_loads = getPlatePointLoad<T, Physics>(nxe, nye, Lx, Ly, Q);
+    T *my_loads = getPlateLoads<T, Physics>(nxe, nye, Lx, Ly, Q);
+
+    auto loads = assembler.createVarsVec(my_loads);
+    assembler.apply_bcs(loads);
+
+    // setup kmat and initial vecs
+    auto kmat = createBsrMat<Assembler, VecType<T>>(assembler);
+    auto soln = assembler.createVarsVec();
+    auto res = assembler.createVarsVec();
+    auto vars = assembler.createVarsVec();
+
+    // assemble the kmat
+    assembler.add_jacobian(res, kmat);
+    assembler.apply_bcs(res);
+    assembler.apply_bcs(kmat);
+
+    // solve the linear system
+    int n_iter = 200, max_iter = 400;
+    // T abs_tol = 1e-11, rel_tol = 1e-14;
+    T abs_tol = 1e-8, rel_tol = 1e-8;
+    // bool print = true;
+    CUSPARSE::GMRES_solve<T>(kmat, loads, soln, n_iter, max_iter, abs_tol, rel_tol, print);
+
+    // print some of the data of host residual
+    auto h_soln = soln.createHostVec();
+    printToVTK<Assembler,HostVec<T>>(assembler, h_soln, "plate.vtk");
+
+    // check the residual of the system
+    assembler.set_variables(soln);
+    assembler.add_residual(res); // internal residual
+    auto rhs = assembler.createVarsVec();
+    CUBLAS::axpy(1.0, loads, rhs);
+    CUBLAS::axpy(-1.0, res, rhs); // rhs = loads - f_int
+    assembler.apply_bcs(rhs);
+    double resid_norm = CUBLAS::get_vec_norm(rhs);
+    printf("resid_norm = %.4e\n", resid_norm);
+}
+
+int main(int argc, char **argv) {
+    // input ----------
+    // bool run_linear = true;
+    int nxe = 40;  // default value
+    double SR = 10.0; // default slenderness
+    double qorder = -1.0; // example values 1.0, 0.5, 0.25
+
+    // Parse arguments
+    for (int i = 1; i < argc; ++i) {
+        char* arg = argv[i];
+        to_lowercase(arg);
+
+        // if (strcmp(arg, "linear") == 0) {
+        //     run_linear = true;
+        // } else if (strcmp(arg, "nonlinear") == 0) {
+        //     run_linear = false;
+        if (strcmp(arg, "--nxe") == 0) {
+            if (i + 1 < argc) {
+                nxe = std::atoi(argv[++i]);
+            } else {
+                std::cerr << "Missing value for --nxe\n";
+                return 1;
+            }
+        } else if (strcmp(arg, "--sr") == 0) {
+            if (i + 1 < argc) {
+                SR = std::atof(argv[++i]);
+            } else {
+                std::cerr << "Missing value for --SR\n";
+                return 1;
+            }
+        } else if (strcmp(arg, "--qorder") == 0) {
+            if (i + 1 < argc) {
+                qorder = std::atof(argv[++i]);
+            } else {
+                std::cerr << "Missing value for --qorder\n";
+                return 1;
+            }
+        } else {
+            std::cerr << "Unknown argument: " << argv[i] << std::endl;
+            std::cerr << "Usage: " << argv[0] << " [--SR value] [--nxe value] [--qorder value]" << std::endl;
+            return 1;
+        }
+    }
+
+    solve_linear(SR, nxe, qorder);
+    return 0;
+};
