0a38bf0bed7e65f5c941134db583a2432f9b6d25
Sean Engelstad <sean.engelstad1@gmail.com>
2025-11-20 09:56:43 -0500

    new assembly case for paper

---
+++ examples/assembly/1_assembly.cu
@@ -0,0 +1,93 @@
+#include "../plate/_src/_plate_utils.h"
+#include "linalg/_linalg.h"
+#include "solvers/_solvers.h"
+#include "mesh/TACSMeshLoader.h"
+#include "mesh/vtk_writer.h"
+#include <chrono>
+
+
+// shell imports
+#include "element/shell/director/linear_rotation.h"
+#include "element/shell/physics/isotropic_shell.h"
+#include "element/shell/basis/lagrange_basis.h"
+#include "element/shell/mitc_shell.h"
+
+void time_assembly(int nxe) {
+    // run the plate problem to time the assembly (nofill)
+    using T = double;   
+
+    CHECK_CUDA(cudaDeviceSynchronize());
+    auto start_nz = std::chrono::high_resolution_clock::now();
+
+    using Quad = QuadLinearQuadrature<T>;
+    using Director = LinearizedRotation<T>;
+    using Basis = LagrangeQuadBasis<T, Quad, 2>;
+    using Geo = Basis::Geo;
+
+    constexpr bool has_ref_axis = false;
+    constexpr bool is_nonlinear = false;
+    using Data = ShellIsotropicData<T, has_ref_axis>;
+    using Physics = IsotropicShell<T, Data, is_nonlinear>;
+    using Assembler = MITCShellAssembler<T, Director, Basis, Physics, DeviceVec, BsrMat>;
+
+    int nye = nxe;
+    double Lx = 1.0, Ly = 1.0, E = 70e9, nu = 0.3, thick = 0.005;
+    auto assembler = createPlateAssembler<Assembler>(nxe, nye, Lx, Ly, E, nu, thick);
+
+    // BSR symbolic factorization
+    auto& bsr_data = assembler.getBsrData();
+    bsr_data.compute_nofill_pattern();
+    assembler.moveBsrDataToDevice();
+
+    // get the loads
+    double Q = 1.0; // load magnitude
+    T *my_loads = getPlateLoads<T, Physics>(nxe, nye, Lx, Ly, Q);
+    auto loads = assembler.createVarsVec(my_loads);
+    assembler.apply_bcs(loads);
+
+    // setup kmat and initial vecs
+    auto kmat = createBsrMat<Assembler, VecType<T>>(assembler);
+    auto soln = assembler.createVarsVec();
+    auto res = assembler.createVarsVec();
+    int nvars = assembler.get_num_vars();
+
+    CHECK_CUDA(cudaDeviceSynchronize());
+    auto end_nz = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> nz_time = end_nz - start_nz;
+    printf("\n=============================\n\n");
+    printf("plate mesh with %d x %d elems or %d NDOF (nofill pattern)\n", nxe, nxe, nvars);
+    printf("\tnonzero pattern in %.5e seconds\n", nz_time.count());
+
+    // assemble the kmat
+    CHECK_CUDA(cudaDeviceSynchronize());
+    auto start_jac = std::chrono::high_resolution_clock::now();
+    assembler.add_jacobian_fast(kmat);
+    assembler.apply_bcs(kmat);
+    CHECK_CUDA(cudaDeviceSynchronize());
+    auto end_jac = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> jac_time = end_jac - start_jac;
+    printf("\tjacobian assembly in %.5e seconds\n", jac_time.count());
+
+    // assemble the residual
+    CHECK_CUDA(cudaDeviceSynchronize());
+    auto start_res = std::chrono::high_resolution_clock::now();
+    assembler.add_residual_fast(res);
+    assembler.apply_bcs(res);
+    CHECK_CUDA(cudaDeviceSynchronize());
+    auto end_res = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> res_time = end_res - start_res;
+    printf("\tresidual assembly in %.5e seconds\n", res_time.count());
+}
+
+int main() {
+    
+    int nxe_vals[3] = {16, 32, 64};
+    // int nxe_vals[7] = {16, 32, 64, 128, 256, 512, 1024};
+
+    for (int i = 0; i < 3; i++) {
+    // for (int i = 0; i < 7; i++) {
+        int nxe = nxe_vals[i];
+        time_assembly(nxe);
+    }
+    // TODO : include other element-types in assembly too? like Hellinger-reissner later?
+};
---
+++ examples/assembly/_cpu_assembly.py
@@ -0,0 +1,109 @@
+# A demonstration of basic functions of the Python interface for TACS,
+# this example goes through the process of setting up the using the
+# tacs assembler directly as opposed to using the pyTACS user interface (see analysis.py):
+# loading a mesh, creating elements, evaluating functions, solution, and output
+# Import necessary libraries
+import numpy as np
+import os, time
+from mpi4py import MPI
+from tacs import TACS, elements, constitutive, functions
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--nxe", type=int, default=16, help="# elements along length (and each direc)")
+args = parser.parse_args()
+
+# first call the plate mesh generator
+os.system(f"python _gen_plate_mesh.py --nxe {args.nxe}")
+
+# Load structural mesh from BDF file
+bdfFile = "plate.bdf"
+tacs_comm = MPI.COMM_WORLD
+struct_mesh = TACS.MeshLoader(tacs_comm)
+struct_mesh.scanBDFFile(bdfFile)
+
+# Set constitutive properties
+rho = 2500.0  # density, kg/m^3
+E = 70e9  # elastic modulus, Pa
+nu = 0.3  # poisson's ratio
+kcorr = 5.0 / 6.0  # shear correction factor
+ys = 350e6  # yield stress, Pa
+min_thickness = 0.002
+max_thickness = 2.0
+# thickness = 0.02
+thickness = 1.0
+
+# Loop over components, creating stiffness and element object for each
+num_components = struct_mesh.getNumComponents()
+for i in range(num_components):
+    descriptor = struct_mesh.getElementDescript(i)
+    # Setup (isotropic) property and constitutive objects
+    prop = constitutive.MaterialProperties(rho=rho, E=E, nu=nu, ys=ys)
+    # Set one thickness dv for every component
+    stiff = constitutive.IsoShellConstitutive(
+        prop, t=thickness, tMin=min_thickness, tMax=max_thickness, tNum=i
+    )
+
+    element = None
+    transform = None
+    if descriptor in ["CQUAD", "CQUADR", "CQUAD4"]:
+        element = elements.Quad4Shell(transform, stiff)
+    struct_mesh.setElement(i, element)
+
+# Create tacs assembler object from mesh loader
+tacs = struct_mesh.createTACS(6)
+
+# Get the design variable values
+x = tacs.createDesignVec()
+x_array = x.getArray()
+tacs.getDesignVars(x)
+
+# Get the node locations
+X = tacs.createNodeVec()
+tacs.getNodes(X)
+tacs.setNodes(X)
+
+# Create the forces
+forces = tacs.createVec()
+force_array = forces.getArray()
+force_array[2::6] += 100.0  # uniform load in z direction
+tacs.applyBCs(forces)
+
+# Set up and solve the analysis problem
+res = tacs.createVec()
+ans = tacs.createVec()
+u = tacs.createVec()
+# default is AMD ordering
+nz_start = time.time()
+mat = tacs.createSchurMat(TACS.RCM_ORDER)
+
+ILUk = 0 # nofill pattern
+# ILUk = 3
+# ILUk = 8
+# ILUk = 1000
+pc = TACS.Pc(mat, lev_fill=ILUk) # for ILU(3), default is full LU or ILU(1000) if not set
+subspace = 100
+# subspace = 200
+restarts = 0
+rtol = 1e-10
+atol = 1e-6
+gmres = TACS.KSM(mat, pc, subspace, restarts)
+gmres.setMonitor(tacs_comm)
+gmres.setTolerances(rtol, atol)
+nz_time = time.time() - nz_start
+
+# Assemble the Jacobian and factor
+assembly_start = time.time()
+alpha = 1.0
+beta = 0.0
+gamma = 0.0
+tacs.zeroVariables()
+tacs.assembleJacobian(alpha, beta, gamma, res, mat)
+assembly_time = time.time() - assembly_start
+
+
+
+tot_time = nz_time + assembly_time + solve_time
+dof = (args.nxe+1)**2 * 6
+if tacs_comm.rank == 0:
+    print(f"{tacs_comm.size}, {args.nxe}, {dof}, {nz_time:.4e}, {assembly_time:.4e}, {solve_time:.4e}, {tot_time:.4e}\n")
